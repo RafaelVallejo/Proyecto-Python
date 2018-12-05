765be0a2c260c78c3d65c6c6cda3c593ff162d34
RafaelVallejo <rafael.vallejo@bec.seguridad.unam.mx>
2018-12-04 23:05:02 -0600

    Add auth_credentials.py

---
+++ auth_credentials.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#UNAM-CERT
+
+consumer_key = 'fwMvqlD0hRs1buC0kb9LTzyLy'
+consumer_secret = 'qRyabXOZ9RtwxZ7t0rIUTU7iDB4BwOVZmitpUkffCNiAsD5XJz'
+
+access_token = '988165664023154688-FcOtrendoHZ4BZEZINb1JKBEHwoClOA'
+access_token_secret = 'mOYHbq7u7pM8BhvCEPiqv5R9zcmnbVsur7vRxDpSrYGth'
